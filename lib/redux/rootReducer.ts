e4e74074c98514548b35d37b6abb0de7e43c4dd8
arda <g.isik.arda@gmail.com>
2024-02-18 18:08:10 +0300

    clean old codes

--- lib/redux/rootReducer.ts
+++ lib/redux/rootReducer.ts
@@ -1,8 +1,6 @@
 /* Instruments */
-import { counterSlice } from "./slices";
 import { productsSlice } from "./slices/productsSlice";
 
 export const reducer = {
-  counter: counterSlice.reducer,
   products: productsSlice.reducer,
 };
